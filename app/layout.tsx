b629f6ac71fea6e8abf15641be61342eaa9568c5
Hans Opoku <88454205+HansOheneba@users.noreply.github.com>
2025-09-24 11:51:22 +0000

    favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,6 +21,36 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "808",
   description: "Next Generation of Bad Decisions",
+
+  icons: [
+    { rel: "icon", url: "/favicon.ico" },
+    {
+      rel: "icon",
+      type: "image/png",
+      sizes: "16x16",
+      url: "/favicon-16x16.png",
+    },
+    {
+      rel: "icon",
+      type: "image/png",
+      sizes: "32x32",
+      url: "/favicon-32x32.png",
+    },
+    { rel: "apple-touch-icon", sizes: "180x180", url: "/apple-touch-icon.png" },
+    {
+      rel: "icon",
+      type: "image/png",
+      sizes: "192x192",
+      url: "/android-chrome-192x192.png",
+    },
+    {
+      rel: "icon",
+      type: "image/png",
+      sizes: "512x512",
+      url: "/android-chrome-512x512.png",
+    },
+    { rel: "manifest", url: "/site.webmanifest" },
+  ],
 };
 
 export default function RootLayout({
